2ce35000d6a46a388aed30d67eee6c7e968cf1b3
Astrid <astrid.idoate@cabify.com>
2017-08-22 12:21:16 +0200

    readableDataSource

--- repositories/domain/repositories/BaseRepository.swift
+++ repositories/domain/repositories/BaseRepository.swift
@@ -1,8 +1,114 @@
 import Foundation
 
-class BaseRepository: ReadableDataSource {
+class BaseRepository<K,V>: ReadableDataSource<K, V> {
+
+    private var readableDataSources = [ReadableDataSource<K, V>]()
+
+    func addReadablaDataSource(readableDataSources: ReadableDataSource<K, V>) {
+        self.readableDataSources.append(readableDataSources)
+    }
 
 
-    override getByKey(key: K) -> V? {}
+    func getByKey(key: K, policy: ReadPolicy) -> V? {
+        var value: V? = nil
 
+        if (policy.useCache()) {
+            value = getValueFromCaches(id: key)
+        }
+
+        if (value == nil && policy.useReadable()) {
+            value = getValueFromReadables(key: key)
+        }
+
+        if (value != nil) {
+            populateCaches(value: value)
+        }
+
+        return value
+    }
+
+    func getAll(policy: ReadPolicy) -> [V]? {
+        var values: [V]? = nil
+
+        if (policy.useCache()) {
+            values = valuesFromCaches
+        }
+
+        if (values == nil && policy.useReadable()) {
+            values = valuesFromReadables
+        }
+
+        if (values != nil) {
+            populateCaches(values: values)
+        }
+
+        return values
+    }
+
+    private func getValueFromCaches(id: K) -> V? {
+        var value: V? = nil
+
+//      for (cacheDataSource in cacheDataSources) {
+//            value = cacheDataSource.getByKey(id)
+//
+//            if (value != nil) {
+//                if (cacheDataSource.isValid(value)) {
+//                    break
+//                } else {
+//                    cacheDataSource.deleteByKey(id)
+//                    value = null
+//                }
+//            }
+//        }
+        return value
+    }
+
+    private func getValueFromReadables(key: K) -> V? {
+        var value: V? = nil
+
+        for readableDataSource in readableDataSources {
+            value = readableDataSource.getByKey(key: key)
+
+            if (value != nil) {
+                break
+            }
+        }
+
+        return value
+    }
+
+    private var valuesFromCaches: [V]? {
+        return nil
+    }
+
+    private var valuesFromReadables: [V]? {
+        var values: [V]? = nil
+
+        for readableDataSource in readableDataSources {
+            values = readableDataSource.getAll()
+
+            if (values != nil) {
+                break
+            }
+        }
+
+        return values
+    }
+
+    private func populateCaches(value: V) {
+    }
+
+    private func populateCaches(values: [V]?) {
+    }
+
+    private func populateCaches(value: V?) {
+    }
+
+    override func getByKey(key: K) -> V? {
+        return getByKey(key: key, policy: ReadPolicy.readAll)
+    }
+
+    override func getAll() -> [V]? {
+        return getAll(policy: ReadPolicy.readAll)
+    }
 }
--- repositories/domain/repositories/DataSource.swift
+++ repositories/domain/repositories/DataSource.swift
@@ -6,13 +6,14 @@
 // @param <K> The class of the key used by this data source.
 // @param <V> The class of the values retrieved from this data source.
 //
+import Foundation
 
 class ReadableDataSource<K, V> {
     func getByKey(key: K) -> V? {
-        return nil
+        fatalError("Readable data source needs getByKey method")
     }
-//
-//    func getAll() -> Collection<Element>? {
-//        return nil
-//    }
+
+    func getAll() -> [V]? {
+        fatalError("Readable data source needs getAll method")
+    }
 }
